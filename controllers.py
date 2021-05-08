91c91f919428902303e6581382b22affaa1e08b4
bernaRocha <mrochabernardo@gmail.com>
2021-05-08 10:03:07 -0300

    Alterações nos arquivos comandos_bd.py controllers.py models.py

--- controllers.py
+++ controllers.py
@@ -1,16 +1,16 @@
 from flask import Flask, jsonify, request
-from models import insert_diretores, get_diretores, update_diretor, delete_diretor, select_diretor
-from serializers import diretores_from_web, diretores_from_db, delete_id_from_web
+from comandos_bd import query, execute
+from models import *
+from serializers import *
 from validacao import valida_diretor, valida_id
 
 
 app = Flask(__name__)
 
-# criar: diretor, gênero, filme e usuário
 # criar enviar dados com json e retornar como json
 
 
-@app.route("/diretores", method=["POST"])
+@app.route("/diretores", methods=["POST"])
 def inserir_diretor():
     diretor = diretores_from_web(**request.json)
     if valida_diretor(**diretor):
@@ -20,7 +20,6 @@ def inserir_diretor():
     else:
         return jsonify({"Erro": "Diretor inválido"})
 
-
 # alterar diretor gênero filme usuário
 
 @app.route("/diretores/<int:id>", methods=["PUT"])
@@ -28,26 +27,24 @@ def alterar_diretor(id):
     diretor = diretores_from_web(**request.json)
     if valida_diretor(**diretor):
         update_diretor(id, **diretor)
- #       diretor_alterado = get_diretores(id)
-        return (None, 201)  ################ ver em aula
+        #       diretor_alterado = get_diretores(id)
+        return jsonify(diretores_from_db(update_diretor))
     else:
         return jsonify({"Erro": "Diretor inválido"})
 
-
 # apagar diretor gênero filme usuário
 # exibir mensagem de erro  ############### # set foreign_key_checks=0;
-@app.route("diretores/<int:id>", methods=["DELETE"])
+@app.route("/diretores/<int:id>", methods=["DELETE"])
 def apagar_diretor(id):
     diretor_id = delete_id_from_web(**request.json)
     try:
         if valida_id(**diretor_id):
             delete_diretor(**diretor_id)
-            return(None,204)
+            return None, 204
     except:
         return jsonify({"Erro": "Diretor inexistente"})
 
 
-# buscar
 @app.route("/diretores", methods=["GET"])
 def buscar_diretor():
     diretor = diretores_from_web(**request.json)
@@ -57,4 +54,69 @@ def buscar_diretor():
     else:
         return jsonify({"Erro": "Não foi possível achar este diretor"})
 
+ #  usuario
 
+@app.route("/usuarios", method=["POST"])  # 1 Checa a rota
+def inserir_usuario():
+    usuario = usuario_from_web(**request.json)
+    if valida_usuario(**usuario):
+        id_usuario = insert_usuario(**usuario)
+        usuario_cadastrado = get_usuario(id_usuario)
+        return jsonify(usuario_from_db(usuario_cadastrado))
+    else:
+        return jsonify({"Erro": "Usuário inválido"})
+
+@app.route("/usuarios", method=["GET"])
+def select_usuario():
+    nome_completo = nome_usuario_from_web(**request.args)
+    usuarios = select_usuario(nome_completo)
+    usuario_from_db = [usuario_from_db(usuarios) for usuario in usuarios]
+
+# generos
+
+@app.route("/generos", methods=["GET"])
+def get_genero():
+    genero = generos_from_web(**request.args)
+    genero = get_genero(nome)
+    generos_from_bd = generos_from_db for nome in generos
+    return jsonify(generos_from_db)
+
+
+@app.route("/generos", methods=["POST"])
+def insert_genero():
+    genero = generos_from_web(**request.json)
+    if valida_genero(**genero):
+        id_genero = insert_genero(**genero)
+        genero_inserido = get_genero(id)
+        return jsonify(genero_from_db(genero_inserido))
+    else:
+        return jsonify({"Erro": "Gênero inválido"})
+
+@app.route("/generos/<int:id>", methods=["PUT"])
+def udate_generos(id):
+    genero = generos_from_web(**request.json)
+    if valida_genero(**genero):
+        update_genero(id, **genero)
+        genero_inserido = get_genero(id)
+        return jsonify(generos_from_db(genero_inserido))
+    else:
+        return jsonify({"Erro": "Gênero inválido"})
+
+@app.route("/generos/<int:id>", methods=["DELETE"])
+def delete_genero(id):
+    try:
+        delete_genero(id)
+        return None, 204
+    except:
+        return jsonify({"Erro": "Gênero não encontrado"})
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
--- validacao.py
+++ validacao.py
@@ -1,3 +1,6 @@
+from models import insert_diretores, get_diretores, update_diretor, delete_diretor, select_diretor
+
+
 def valida_diretor(nome_completo):
     if len(nome_completo) == 0:
         return False
@@ -10,3 +13,10 @@ def valida_id(id):
         return False
     else:
         return True
+
+def valida_usuario(nome_completo, CPF):
+    if len(nome_completo) == 0:
+        return False
+    if len(CPF) != 14:
+        return False
+    return True
